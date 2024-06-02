11ffc3db158e1a0ed1dc80d127691dd821d86826
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2024-06-02 10:45:41 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Navbar from './Component/Navbar/navbar';
+import HeroSlider from './Component/Hero/hero';
+import SearchHospital from './Component/Search/searchhospital';
+import FAQs from './Component/FAQS/faqs';
+import HeroServices from './Component/Service/service';
+import Offers from './Component/Offer/offer';
+import Specialization from './Component/Specialisation/specialisatiion';
+import Specialists from './Component/Specialist/specialist';
+import PatientCaring from './Component/Patientcare/patientcare';
+import Blogs from './Component/Blog/blog';
+import OurFamilies from './Component/OurFamily/ourfamily';
+import DownloadApp from './Component/DownloadOurApp/downloadapp';
+import Footer from "./Component/Footer/footer";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar/>
+              <HeroSlider />
+              <SearchHospital/>
+              <HeroServices/>
+              <Offers/>
+              <Specialization/>
+              <Specialists/>
+              <PatientCaring/>
+              <Blogs/>
+              <OurFamilies/>
+              <DownloadApp/>
+              <FAQs/>
+              <Footer/>
+            </>
+          }/>
+        
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
